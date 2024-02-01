001b8c929ce5f7e5902793a9d3def03b3a228fb1
Pedro Rocha <pedro2022bd@icloud.com>
2024-02-01 15:47:30 -0300

    ajuda

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+
+const dataDoEvento = new Date("Fev 10, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMS = 1000 * 60 * 60 * 24;
+    const HoraEmMs = 1000 * 60 * 60;
+    const MinutoEmMs = 1000 * 60;
+
+    const diasAteOEvento= Math.floor(distanciaAteOEvento / diaEmMS);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMS ) / HoraEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento  % HoraEmMs) / MinutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % MinutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+}, 1000);
+
+
+
+
+
